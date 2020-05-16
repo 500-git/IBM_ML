fa61f67395d0f81b2af958711870a2fe58ecb476
500 <qq953406331@gmail.com>
2020-05-17 02:06:14 +0900

    20-05-17

--- 05-KNN.py
+++ 05-KNN.py
@@ -16,5 +16,73 @@ df.head()
 
 print(df["custcat"].value_counts())
 
-df.hist(colum = "income", bins = 50)
+df.hist(column = "income", bins = 50)
 
+df.columns
+
+x = df[["region","tenure","age","marital","address","income","ed","employ","retire","gender","reside"]].values
+print(x[0:5],"\n")
+
+y = df["custcat"].values
+print(y[0:5],"\n")
+
+x = preprocessing.StandardScaler().fit(x).transform(x.astype(float))
+print(x[0:5],"\n")
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state = 4)
+print("Train set:",x_train.shape,y_train.shape)
+print("Test set:",x_test.shape,y_test.shape)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+print("========= K = 4  =========")
+k = 4
+
+neigh = KNeighborsClassifier(n_neighbors = k).fit(x_train,y_train)
+
+yhat = neigh.predict(x_test)
+print(yhat[0:5],"\n")
+
+from sklearn import metrics
+print("Trian set Accuracy: {0}".format(metrics.accuracy_score(y_train,neigh.predict(x_train))))
+print("Test set Accuracy: {0}".format(metrics.accuracy_score(y_test,neigh.predict(x_test))))
+
+print("========= K = 6  =========")
+k = 6
+
+neigh6 = KNeighborsClassifier(n_neighbors = k).fit(x_train,y_train)
+
+yhat6 = neigh6.predict(x_test)
+print(yhat6[0:5],"\n")
+
+from sklearn import metrics
+print("Trian set Accuracy: {0}".format(metrics.accuracy_score(y_train,neigh6.predict(x_train))))
+print("Test set Accuracy: {0}".format(metrics.accuracy_score(y_test,neigh6.predict(x_test))))
+
+Ks = 10
+
+mean_acc = np.zeros((Ks - 1))
+std_acc = np.zeros((Ks -1))
+
+ConfusionMx = [];
+
+for n in range(1,Ks):
+    neigh = KNeighborsClassifier(n_neighbors = n).fit(x_train,y_train)
+    yhat = neigh.predict(x_test)
+    mean_acc[n - 1] = metrics.accuracy_score(y_test,yhat)
+
+    std_acc[n - 1] = np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+print("mean acc:",mean_acc)
+
+plt.plot(range(1,Ks),mean_acc,"g")
+plt.fill_between(range(1,Ks),mean_acc - 1 *std_acc,mean_acc +1 * std_acc,alpha = 0.10)
+plt.legend(("Accuracy","+/- 3xstd"))
+plt.ylabel("Accuracy")
+plt.xlabel("Number of Nabors (K)")
+plt.tight_layout()
+plt.show()
+
+print("Best accuracy: {0}, best k: {1}".format(mean_acc.max(),mean_acc.argmax()+1))
--- 06_decision_tree.py
+++ 06_decision_tree.py
@@ -1,4 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 
+my_data = pd.read_csv("drug200.csv",delimiter = ",")
+#  print(my_data[0:5])
+#  print(len(my_data),"\n")
+
+x = my_data[["Age","Sex","BP","Cholesterol","Na_to_K"]].values
+#  print(x[0:5],"\n")
+
+from sklearn import preprocessing
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(["F","M"])
+x[:,1] = le_sex.transform(x[:,1]) #replace "sex" with int number
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit(["LOW","NORMAL","HIGH"])
+x[:,2] = le_BP.transform(x[:,2]) #replace "BP" with int number
+
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit(["NORMAL","HIGH"])
+x[:,3] = le_Chol.transform(x[:,3])
+#  print(x[0:5])
+
+y = my_data["Drug"]
+#  print(y)
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.3,random_state = 3)
+#  print("Train set: ",x_train.shape,"\n",y_train.shape,"\n")
+#  print("Test set: ",x_test,y_test,"\n")
+#  print(y_test.values)
+
+drugTree = DecisionTreeClassifier(criterion = "entropy",max_depth = 4)
+#  print(drugTree)
+
+drugTree.fit(x_train,y_train)
+
+predTree = drugTree.predict(x_test)
+
+#  print(predTree[0:10])
+#  print(y_test[0:10])
+
+from sklearn import metrics
+
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
+print("DecisionTrees's Accuracy: ",metrics.accuracy_score(y_test,predTree))
+
+correct = 0
+for x in range(0,len(predTree)):
+    if predTree[x] == y_test.values[x]:
+        correct += 1
+
+print("manual acc_score = ",correct/len(predTree))
+
+from sklearn.externals.six import StringIO
+import pydotplus
+import matplotlib.image as mpimg
+from sklearn import tree
+
+dot_data = StringIO()
+filename = "drugtree.png"
+featureNames = my_data.columns[0:5]
+targetNames = my_data["Drug"].unique().tolist()
+out = tree.export_graphviz(drugTree,feature_names = featureNames,out_file = dot_data,class_names = np.unique(y_train),filled = True,special_characters = True,rotate = False)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png(filename)
+img = mpimg.imread(filename)
+plt.figure(figsize = (100,200))
+plt.imshow(img,interpolation = "nearest")
